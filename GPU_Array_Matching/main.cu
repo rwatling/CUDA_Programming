a1c2cec6b9431dc043458d870ba62e9532968a94
Robert Watling <rwatling0902@gmail.com>
2021-02-05 12:42:03 -0500

    Small array matching example executing correctly

--- GPU_Array_Matching/main.cu
+++ GPU_Array_Matching/main.cu
@@ -3,21 +3,40 @@
 * Author: Robbie Watling
 */
 
-# include "system_includes.h"
+# include <cuda.h>
+# include <cuda_runtime_api.h>
+# include <device_launch_parameters.h>
+# include <iostream>
+# include <vector>
+# include <utility>
+# include <iostream>
 
-__global__ void array_search(int* a, int* b, int* match) {
-	//a[thread_id] = b[thread_id] return match
+__global__ void array_search(int* a, int* b, int* match, int n) {
+	
+	int thread_id = (blockIdx.x * blockDim.x) + threadIdx.x;
+	int val;
+
+	if (thread_id < n) {
+		
+		val = a[thread_id];
+
+		for (int i = 0; i < n; i++) {
+			if (b[i] == val) {
+				*match = 1;
+				break;
+			}
+		}
+	}
 }
-
 
 using namespace std;
 
 int main() {
 
 	/***Variable Declarations***/
-	//Array size
 	int N;
-	int num_states;
+	int host_match;
+	int* dev_match;
 
 	//Host int arrays
 	vector<int> host_a;
@@ -30,13 +49,18 @@ int main() {
 	int* dev_c;
 
 	//Memory variables
-	size_t bytes;
+	size_t arr_bytes;
+	size_t match_bytes;
+	int NUM_THREADS;
+	int NUM_BLOCKS;
 
 	/***Initialize data structures***/
-	num_states = 3;
+	N = 1 << 5;
 
-	//Initialize host arrays
-	N = 1 << 4;
+	// "Match" int allocation
+	host_match = 1;
+	match_bytes = (size_t) 1 * sizeof(int);
+	cudaMalloc((void**) &dev_match, match_bytes);
 
 	//Vector a
 	for (int i = 0; i < N; i++) {
@@ -45,30 +69,51 @@ int main() {
 
 	//Vector b: Will have matches with even elements of a
 	for (int i = 0; i < N; i++) {
-		if (i % 2 == 0) {
-			host_b.push_back(i);
-		}
-		else {
-			host_b.push_back(0);
-		}
+		host_b.push_back(i);
 	}
 
 	//Vector c: Will have no matches with a or b
 	for (int i = 0; i < N; i++) {
-		host_c.push_back((i + 1) * 2);
+		host_c.push_back(-1);
 	}
 
 	//Int arrays of size N for device
-	N = 1 << 4;
-	bytes = N * sizeof(int);
-	cudaMalloc((void**) &dev_a, bytes);
-	cudaMalloc((void**) &dev_b, bytes);
-	cudaMalloc((void**) &dev_c, bytes);
+	arr_bytes = N * sizeof(int);
+	cudaMalloc((void**) &dev_a, arr_bytes);
+	cudaMalloc((void**) &dev_b, arr_bytes);
+	cudaMalloc((void**)&dev_c, arr_bytes);
+
+	/*** Copy arrays to device ***/
+	cudaMemcpy(dev_a, host_a.data(), arr_bytes, cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_b, host_b.data(), arr_bytes, cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_c, host_c.data(), arr_bytes, cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_match, &host_match, match_bytes, cudaMemcpyHostToDevice);
+
+	/*** Thread setup ***/
+	NUM_THREADS = 1 << 3;
+	NUM_BLOCKS = (N + NUM_THREADS - 1) / NUM_THREADS;
+
+	//Search arrays and copy result back to host
+	//Memcopy works as a synchronization layer
+	array_search <<<NUM_BLOCKS, NUM_THREADS >>> (dev_a, dev_b, dev_match, N);
+	cudaMemcpy(&host_match, dev_match, match_bytes, cudaMemcpyDeviceToHost);
+
+	cout << "Search array A and B result: " << host_match << endl;
+
+	//Zero out match
+	cudaMemset((void*) dev_match, 0, match_bytes);
+
+	//Search arrays ad copy result back to host
+	array_search << <NUM_BLOCKS, NUM_THREADS >> > (dev_b, dev_c, dev_match, N);
+	cudaMemcpy(&host_match, dev_match, match_bytes, cudaMemcpyDeviceToHost);
+
+	cout << "Search array B and C result: " << host_match << endl;
 
 	/***Free variables***/
 	cudaFree(dev_a);
 	cudaFree(dev_b);
 	cudaFree(dev_c);
+	cudaFree(dev_match);
 
 	return 0;
 }
