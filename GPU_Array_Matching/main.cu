c1fe0c6a815c9742384e75dc2d5ad5c1113ffbb5
Robert Watling <rwatling0902@gmail.com>
2021-02-13 21:52:41 -0500

    restructured code after meeting with JQ. Only missing implementation of actual algorithm

--- GPU_Array_Matching/main.cu
+++ GPU_Array_Matching/main.cu
@@ -11,19 +11,11 @@
 # include <utility>
 # include <iostream>
 
-typedef struct {
-	int* arr1;
-	int* arr2;
-	int match;
-	int size;
-} Array_Comp_Struct;
-
-__global__ void array_match(int* a, int* b, int* match, int n) {
-	
+__global__ void array_match(int* all_arrays, int* match_array, int size) {
 	int thread_id = (blockIdx.x * blockDim.x) + threadIdx.x;
-
-	if (thread_id < n) {
-		*match = ((int) a[thread_id] == b[thread_id]);
+	
+	if (thread_id > 0 && thread_id < size) {
+		//if member can be found in thread_id-1 update match
 	}
 }
 
@@ -32,88 +24,77 @@ using namespace std;
 int main() {
 
 	/***Variable Declarations***/
-
-	// Supervising variables
-	int N;
+	int* host_arrays;
+	int* device_arrays;
+	int* host_match;
+	int* device_match;
+	int array_size;
+	int match_size;
+	int num_arrays;
 	int NUM_THREADS;
 	int NUM_BLOCKS;
-	size_t byte;
+	size_t one_t;
+	size_t array_set_bytes;
+	size_t match_bytes;
 	cudaError cuda_err;
 
-	//Host variables
-	//vector<int> host_a;
-	//vector<int> host_b;
-	//vector<int> host_c;
-	//int host_match;
-	Array_Comp_Struct* host_data;
-	size_t host_struct_size;
-
-	//Device variables
-	//int* dev_a;
-	//int* dev_b;
-	//int* dev_c;
-	Array_Comp_Struct* device_data;
-	size_t device_struct_size;
-
 	/***Initialization***/
-	N = 1 << 5;
+	array_size = 1 << 4;
+	num_arrays = 1 << 6;
+	match_size = num_arrays;
+	NUM_THREADS = num_arrays;
+	NUM_BLOCKS = 1;
 
-	// Struct for host information. 
-	// Members: Two arrays of size N, integer N, and integer match
-	byte = (size_t) 1;
-	host_struct_size = (size_t) (N * 2 * sizeof(int)) + sizeof(int) + sizeof(int);
-	host_data = (Array_Comp_Struct*) calloc(byte, host_struct_size);
+	// Host allocation
+	one_t = (size_t) 1;
+	array_set_bytes = (size_t) num_arrays * array_size * sizeof(int);
+	match_bytes = (size_t) match_size * sizeof(int);
 
-	if (host_data == NULL) {
-		cerr << "HOST DATA IS NULL" << endl;
+	host_arrays = (int*) calloc(one_t, array_set_bytes);
+	host_match = (int*) calloc(one_t, match_bytes);
+
+	if (host_arrays == NULL) {
+		cerr << "Host arrays calloc failed\n" << endl;
+		return -1;
 	}
 
-	// Struct for GPU information. 
-	// Members: Two arrays of size N, integer N, and integer match
-	device_struct_size = (size_t) (N * 2 * sizeof(int)) + sizeof(int) + sizeof(int);
-	cuda_err = cudaMalloc((void**) &device_data, device_struct_size);
+	if (host_match == NULL) {
+		cerr << "Host match calloc failed\n" << endl;
+		return -1;
+	}
+
+	//Device Allocation
+	cuda_err = cudaMalloc((void**)&device_arrays, array_set_bytes);
 
 	if (cuda_err != cudaSuccess) {
-		cerr << "CUDA MALLOC FAILED" << endl;
+		cerr << "Device allocation for array set failed\n" << endl;
+		return -1;
 	}
 
+	cuda_err = cudaMalloc((void**)&device_match, match_bytes);
 
-	//Vector a
-
-	//Vector b: Will have matches with even elements of a
-
-	//Vector c: Will have no matches with a or b
+	if (cuda_err != cudaSuccess) {
+		cerr << "Device allcoation for match array failed\n" << endl;
+		return -1;
+	}
 
 
 	/*** Copy arrays to device ***/
-	//cudaMemcpy(dev_a, host_a.data(), arr_bytes, cudaMemcpyHostToDevice);
-	//cudaMemcpy(dev_b, host_b.data(), arr_bytes, cudaMemcpyHostToDevice);
-	//cudaMemcpy(dev_c, host_c.data(), arr_bytes, cudaMemcpyHostToDevice);
-	//cudaMemcpy(dev_match, &host_match, match_bytes, cudaMemcpyHostToDevice);
-
-	/*** Thread setup ***/
-	NUM_THREADS = N;
-	NUM_BLOCKS = (N + NUM_THREADS - 1) / NUM_THREADS;
+	cudaMemcpy((void*)device_arrays, (void*)host_arrays, array_set_bytes, cudaMemcpyHostToDevice);
+	cudaMemcpy((void*)device_match, (void*)host_match, match_bytes, cudaMemcpyHostToDevice);
 
 	//Search arrays and copy result back to host
 	//Memcopy works as a synchronization layer
 	//array_match <<<NUM_BLOCKS, NUM_THREADS >>> (dev_a, dev_b, dev_match, N);
+	
+	//Copy match back to host
 	//cudaMemcpy(&host_match, dev_match, match_bytes, cudaMemcpyDeviceToHost);
-
-	//cout << "Search array A and B result: " << host_match << endl;
-
-	//Zero out match
-	//cudaMemset((void*) dev_match, 0, match_bytes);
-
-	//Search arrays ad copy result back to host
-	//array_match << <NUM_BLOCKS, NUM_THREADS >> > (dev_b, dev_c, dev_match, N);
-	//cudaMemcpy(&host_match, dev_match, match_bytes, cudaMemcpyDeviceToHost);
-
-	//cout << "Search array B and C result: " << host_match << endl;
 
 	/***Free variables***/
-	cudaFree(device_data);
-	free(host_data);
+	cudaFree(device_arrays);
+	cudaFree(device_match);
+	free(host_arrays);
+	free(host_match);
 
 	return 0;
 }
