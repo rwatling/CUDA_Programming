bb1d0978459b44bff250b32fd273a613fec4daa9
Robbie Watling <rwatling@pop-os.localdomain>
2021-03-01 13:46:01 -0500

    updated random to execute in match loop. Moved array match to separate file

---
+++ GPU_Array_Matching/array_match.cu
@@ -0,0 +1,43 @@
+#include "array_match.h"
+
+__global__ void array_match(int* all_arrays, int* match_array, int num_arrays,  int size) {
+	int thread_id = (blockIdx.x * blockDim.x) + threadIdx.x;
+
+	//For random number generation
+	int maxRand = 100;
+	curandState state;
+	unsigned long long seed = clock();
+
+	curand_init(seed + thread_id, 0, 0, &state);
+
+	if (thread_id < num_arrays) {
+		int* current_array = all_arrays + (thread_id * size); //Pointer arithmetic
+		int* prev_array = all_arrays + ((thread_id - 1) * size); //Pointer arithmetic
+		int match = 0;
+
+		if (thread_id > 0) {
+
+			for (int i = 0; i < size; i++) {
+				for (int j = 0; j < size; j++) {
+
+					//At runtime moment, generate random number
+					int rand_num = (int) (curand_uniform(&state) * maxRand);;
+					current_array[i] = rand_num;
+
+					//Check if previous match
+					if (rand_num == prev_array[j]) {
+						match = 1;
+					}
+				}
+			}
+
+			match_array[thread_id] = match;
+
+		} else if (thread_id == 0) {
+			for (int i = 0; i < size; i++) {
+				//At runtime moment, generate random number
+				current_array[i] = (int) (curand_uniform(&state) * maxRand);
+			}
+		}
+	}
+}
--- GPU_Array_Matching/main.cu
+++ GPU_Array_Matching/main.cu
@@ -1,34 +1,15 @@
 /* This program searches "states" for matches in their arrays using CUDA
-*  
+*
 * Author: Robbie Watling
 */
 
 #include "cuda_includes.h"
-#include "rand_init.h"
+#include "array_match.h"
 #include <iostream>
 #include <vector>
 #include <utility>
 #include <iostream>
-
-__global__ void array_match(int* all_arrays, int* match_array, int num_arrays,  int size) {
-	int thread_id = (blockIdx.x * blockDim.x) + threadIdx.x;
-
-	if (thread_id > 0 && thread_id < num_arrays) {
-		int* current_array = all_arrays + (thread_id * size); //Pointer arithmetic
-		int* prev_array = all_arrays + ((thread_id - 1) * size); //Pointer arithmetic
-		int match = 0;
-
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-				if (current_array[i] == prev_array[j]) {
-				match = 1;
-				}
-			}
-		}
-
-		match_array[thread_id] = match;
-	}
-}
+#include <ctime>
 
 using namespace std;
 
@@ -48,8 +29,6 @@ int main() {
 	size_t array_set_bytes;
 	size_t match_bytes;
 	cudaError cuda_err;
-	//curandStatus_t rand_err;
-	//curandGenerator_t gen;
 
 	/***Initialization***/
 	array_size = 1 << 3;
@@ -66,7 +45,7 @@ int main() {
 	host_arrays = (int*) calloc(one_t, array_set_bytes);
 	host_match = (int*) calloc(one_t, match_bytes);
 
-	
+
 	if (host_arrays == NULL) {
 		cerr << "Host arrays calloc failed\n" << endl;
 		return -1;
@@ -92,26 +71,14 @@ int main() {
 		return -1;
 	}
 
-	/*
-	for (int i = 0; i < num_arrays; i++) {
-		int step = i * array_size;
-		int value = i / 2;
-
-		for (int j = 0; j < array_size; j++) {
-			host_arrays[step + j] = value;
-		}
-	}*/
-
-	rand_init <<<NUM_BLOCKS, NUM_THREADS >>> (device_arrays, num_arrays, array_size);
-
 	/*** Copy arrays to device ***/
 	//cudaMemcpy((void*)device_arrays, (void*)host_arrays, array_set_bytes, cudaMemcpyHostToDevice);
 	//cudaMemcpy((void*)device_match, (void*)host_match, match_bytes, cudaMemcpyHostToDevice);
-	
+
 	/*** Search arrays and copy result back to host ***/
 	//Memcopy works as a synchronization layer
 	array_match <<<NUM_BLOCKS, NUM_THREADS >>> (device_arrays, device_match, num_arrays, array_size);
-	
+
 	//Copy match back to host
 	cudaMemcpy(host_match, device_match, match_bytes, cudaMemcpyDeviceToHost);
 
@@ -141,7 +108,7 @@ int main() {
 	/***Free variables***/
 	cudaFree(device_arrays);
 	cudaFree(device_match);
-	//free(host_arrays);
+	free(host_arrays);
 	free(host_match);
 
 	return 0;
