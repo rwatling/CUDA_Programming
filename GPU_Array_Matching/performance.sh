59da89592ceae95cb59f5231187e737c67121bfc
Robbie Watling <rwatling@pop-os.localdomain>
2021-03-12 21:40:20 -0500

    fix performance script parameters

--- GPU_Array_Matching/performance.sh
+++ GPU_Array_Matching/performance.sh
@@ -3,10 +3,13 @@
 # array_size goes from 3 to 8096
 for i in {3..13}
 do	
-	# num_arrays goes from 8 to 1024
-	for j in {3..10}
-	do		
-		./main $((2**i)) $((2**j)) 1 >> results.csv
-		./main $((2**i)) $((2**j)) 0 >> results.csv
-	done
+	./main $((2**i)) 256 1 >> results.csv
+	./main $((2**i)) 256 0 >> results.csv
+done
+
+# num_arrays goes from 8 to 1024
+for j in {3..10}
+do		
+	./main 64 $((2**j)) 1 >> results.csv
+	./main 64 $((2**j)) 0 >> results.csv
 done
