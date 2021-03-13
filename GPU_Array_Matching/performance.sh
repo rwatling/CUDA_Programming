2d7b989c349bb04106e1bc17296b03e245a56611
Robbie Watling <rwatling@pop-os.localdomain>
2021-03-12 21:18:50 -0500

    setup round 2

--- GPU_Array_Matching/performance.sh
+++ GPU_Array_Matching/performance.sh
@@ -6,11 +6,7 @@ do
 	# num_arrays goes from 8 to 1024
 	for j in {3..10}
 	do		
-		# Run the same parameters 4 times
-		for k in {1..4}
-		do
-			./main $((2**i)) $((2**j)) 1 >> results.csv
-			./main $((2**i)) $((2**j)) 0 >> results.csv
-		done
+		./main $((2**i)) $((2**j)) 1 >> results.csv
+		./main $((2**i)) $((2**j)) 0 >> results.csv
 	done
 done
