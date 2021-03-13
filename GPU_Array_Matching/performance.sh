a07b604eeeb9f63f715ce00d63ec99a3d87a893f
Robbie <rwatling0902@gmail.com>
2021-03-12 17:30:41 -0800

    Added base version of csv

--- GPU_Array_Matching/performance.sh
+++ GPU_Array_Matching/performance.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# array_size
-for i in {3..14}
+# array_size goes from 3 to 8096
+for i in {3..13}
 do	
-	# num_arrays
+	# num_arrays goes from 8 to 1024
 	for j in {3..10}
 	do		
-		# Run the same parameters 8 times
-		for k in {1..8}
+		# Run the same parameters 4 times
+		for k in {1..4}
 		do
 			./main $((2**i)) $((2**j)) 1 >> results.csv
 			./main $((2**i)) $((2**j)) 0 >> results.csv
