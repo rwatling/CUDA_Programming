9d7013617eb784ba471a66006ed2e8ed6d90f6b3
Robbie Watling <rwatling@pop-os.localdomain>
2021-03-15 11:31:20 -0400

    Old version of analysis had mismatch in dataframes. Corrected.

--- GPU_Array_Matching/analysis.R
+++ GPU_Array_Matching/analysis.R
@@ -7,70 +7,15 @@ file4 <- "data/change_num_arr_small.csv"
 file5 <- "data/change_num_arr_avg.csv"
 file6 <- "data/change_num_arr_lg.csv"
 
-png1 <- "data/change_arr_size_small.png"
-png2 <- "data/change_arr_size_avg.png"
-png3 <- "data/change_arr_size_lg.png"
-png4 <- "data/change_num_arr_small.png"
-png5 <- "data/change_num_arr_avg.png"
-png6 <- "data/change_num_arr_lg.png"
-
-### File 1: Small number of arrays, change array size ###
-changeArrSizeDf <- read.csv(file1)
-sharedChangeArrSizeDf <- changeArrSizeDf[which(changeArrSizeDf$type == 1),]
-globalChangeArrSizeDf <- changeArrSizeDf[which(changeArrSizeDf$type == 0),]
-
-png(png1)
-
-plot(globalChangeArrSizeDf$array_size, globalChangeArrSizeDf$time, 
-     col="red", xlab="Array Size", ylab = "Time", main = "Time vs. Array Size", pch =19)
-points(sharedChangeArrSizeDf$array_size, sharedChangeArrSizeDf$time, col="blue", pch =19)
-
-lines(sharedChangeArrSizeDf$array_size, sharedChangeArrSizeDf$time, col="blue")
-lines(globalChangeArrSizeDf$array_size, globalChangeArrSizeDf$time, col="red")
-
-fit <- lm(globalChangeArrSizeDf$time ~ globalChangeArrSizeDf$array_size)
-abline(fit)
-
-legend("topleft", legend=c("Global Mem", "Shared Mem", "Global Fit"),col=c("red", "blue", "black"), pch = c(19, 19, 19))
-
-dev.off()
-
-#speed up
-mean(globalChangeArrSizeDf$time / sharedChangeArrSizeDf$time)
-
-### File 2: Average number of arrays, change array size###
-changeArrSizeDf <- read.csv(file2)
-sharedChangeArrSizeDf <- changeArrSizeDf[which(changeArrSizeDf$type == 1),]
-globalChangeArrSizeDf <- changeArrSizeDf[which(changeArrSizeDf$type == 0),]
-
-png(png2)
-
-plot(globalChangeArrSizeDf$array_size, globalChangeArrSizeDf$time, 
-     col="red", xlab="Array Size", ylab = "Time", main = "Time vs. Array Size", pch =19)
-points(sharedChangeArrSizeDf$array_size, sharedChangeArrSizeDf$time, col="blue", pch =19)
-
-lines(sharedChangeArrSizeDf$array_size, sharedChangeArrSizeDf$time, col="blue")
-lines(globalChangeArrSizeDf$array_size, globalChangeArrSizeDf$time, col="red")
-
-fit <- lm(globalChangeArrSizeDf$time ~ globalChangeArrSizeDf$array_size)
-abline(fit)
-
-legend("topleft", legend=c("Global Mem", "Shared Mem", "Global Fit"),col=c("red", "blue", "black"), pch = c(19, 19, 19))
-
-dev.off()
-
-#speed up
-mean(globalChangeArrSizeDf$time / sharedChangeArrSizeDf$time)
-
-### File 3: Large number of arrays, change array size###
+### Change Array Size: file1-3###
 changeArrSizeDf <- read.csv(file3)
 sharedChangeArrSizeDf <- changeArrSizeDf[which(changeArrSizeDf$type == 1),]
 globalChangeArrSizeDf <- changeArrSizeDf[which(changeArrSizeDf$type == 0),]
 
-png(png3)
-
+plot.new()
 plot(globalChangeArrSizeDf$array_size, globalChangeArrSizeDf$time, 
-     col="red", xlab="Array Size", ylab = "Time", main = "Time vs. Array Size", pch =19)
+     col="red", xlab="Array Size", ylab = "Time", main = "Time vs. Array Size", pch =19,
+     ylim=c(min(sharedChangeArrSizeDf$time), max(globalChangeArrSizeDf$time) + 0.005))
 points(sharedChangeArrSizeDf$array_size, sharedChangeArrSizeDf$time, col="blue", pch =19)
 
 lines(sharedChangeArrSizeDf$array_size, sharedChangeArrSizeDf$time, col="blue")
@@ -81,79 +26,28 @@ abline(fit)
 
 legend("topleft", legend=c("Global Mem", "Shared Mem", "Global Fit"),col=c("red", "blue", "black"), pch = c(19, 19, 19))
 
-dev.off()
-
 #speed up
 mean(globalChangeArrSizeDf$time / sharedChangeArrSizeDf$time)
 
-### File 4: Small array size, change number of arrays ###
+### Change number of arrays: files4-6 ###
 changeArrNumDf <- read.csv(file4)
-sharedChangeArrNumDf <- changeArrSizeDf[which(changeArrNumDf$type == 1),]
-globalChangeArrNumDf <- changeArrSizeDf[which(changeArrNumDf$type == 0),]
+sharedChangeArrNumDf <- changeArrNumDf[which(changeArrNumDf$type == 1),]
+globalChangeArrNumDf <- changeArrNumDf[which(changeArrNumDf$type == 0),]
 
-png(png4)
+plot.new()
+plot(globalChangeArrNumDf$number_of_arrays, globalChangeArrNumDf$time, 
+     col="red", xlab="Array Size", ylab = "Time", main = "Time vs. Array Number", pch =19,
+     ylim=c(min(sharedChangeArrNumDf$time), max(globalChangeArrNumDf$time) + 0.005))
+points(sharedChangeArrNumDf$number_of_arrays, sharedChangeArrNumDf$time, col="blue", pch =19)
 
-plot(globalChangeArrNumDf$array_size, globalChangeArrNumDf$time, 
-     col="red", xlab="Array Size", ylab = "Time", main = "Time vs. Array Number", pch =19)
-points(sharedChangeArrNumDf$array_size, sharedChangeArrNumDf$time, col="blue", pch =19)
+lines(sharedChangeArrNumDf$number_of_arrays, sharedChangeArrNumDf$time, col="blue")
+lines(globalChangeArrNumDf$number_of_arrays, globalChangeArrNumDf$time, col="red")
 
-lines(sharedChangeArrNumDf$array_size, sharedChangeArrNumDf$time, col="blue")
-lines(globalChangeArrNumDf$array_size, globalChangeArrNumDf$time, col="red")
-
-fit <- lm(globalChangeArrNumDf$time ~ globalChangeArrNumDf$array_num)
+fit <- lm(globalChangeArrNumDf$time ~ globalChangeArrNumDf$number_of_arrays)
 abline(fit)
 
 legend("topleft", legend=c("Global Mem", "Shared Mem", "Global Fit"),col=c("red", "blue", "black"), pch = c(19, 19, 19))
 
-dev.off()
-
 #speed up
 mean(globalChangeArrNumDf$time / sharedChangeArrNumDf$time)
 
-### File 5: Average array size, change number of arrays ###
-changeArrNumDf <- read.csv(file5)
-sharedChangeArrNumDf <- changeArrSizeDf[which(changeArrNumDf$type == 1),]
-globalChangeArrNumDf <- changeArrSizeDf[which(changeArrNumDf$type == 0),]
-
-png(png5)
-
-plot(globalChangeArrNumDf$array_size, globalChangeArrNumDf$time, 
-     col="red", xlab="Array Size", ylab = "Time", main = "Time vs. Array Number", pch =19)
-points(sharedChangeArrNumDf$array_size, sharedChangeArrNumDf$time, col="blue", pch =19)
-
-lines(sharedChangeArrNumDf$array_size, sharedChangeArrNumDf$time, col="blue")
-lines(globalChangeArrNumDf$array_size, globalChangeArrNumDf$time, col="red")
-
-fit <- lm(globalChangeArrNumDf$time ~ globalChangeArrNumDf$array_num)
-abline(fit)
-
-legend("topleft", legend=c("Global Mem", "Shared Mem", "Global Fit"),col=c("red", "blue", "black"), pch = c(19, 19, 19))
-
-dev.off()
-
-#speed up
-mean(globalChangeArrNumDf$time / sharedChangeArrNumDf$time)
-
-### File 6: Large array size, change number of arrays ###
-changeArrNumDf <- read.csv(file6)
-sharedChangeArrNumDf <- changeArrSizeDf[which(changeArrNumDf$type == 1),]
-globalChangeArrNumDf <- changeArrSizeDf[which(changeArrNumDf$type == 0),]
-
-png(png6)
-
-plot(globalChangeArrNumDf$array_size, globalChangeArrNumDf$time, 
-     col="red", xlab="Array Size", ylab = "Time", main = "Time vs. Array Number", pch =19)
-points(sharedChangeArrNumDf$array_size, sharedChangeArrNumDf$time, col="blue", pch =19)
-
-lines(sharedChangeArrNumDf$array_size, sharedChangeArrNumDf$time, col="blue")
-lines(globalChangeArrNumDf$array_size, globalChangeArrNumDf$time, col="red")
-
-fit <- lm(globalChangeArrNumDf$time ~ globalChangeArrNumDf$array_num)
-abline(fit)
-
-legend("topleft", legend=c("Global Mem", "Shared Mem", "Global Fit"),col=c("red", "blue", "black"), pch = c(19, 19, 19))
-
-dev.off()
-
-#speed up
-mean(globalChangeArrNumDf$time / sharedChangeArrNumDf$time)
