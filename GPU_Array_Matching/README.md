0eb47e2bf32d4f27b2a77930f9237d57772bd0ea
Robbie Watling <rwatling@pop-os.localdomain>
2021-03-23 20:22:52 -0400

    Updated readme and analysis.R

--- GPU_Array_Matching/README.md
+++ GPU_Array_Matching/README.md
@@ -1,9 +1,16 @@
 # GPU Array Matching
 
-Robbie Watling
+Author: Robbie Watling
+
 Independent Study with Dr. Junqiao Qiu Spring '21
 
 ## Description
+This program performs an array matching experiment on a NVIDIA capable GPU. A specified number of arrays are allocated at the beginning of the program of a specified size. These arrays are then copied to a NVIDIA device where each array is operated on by a GPU thread. Note that the current implementation uses only one thread block and up to 1024 individual threads. The thread then generates a random number for its array and stores it in either global memory or shared memory on the device. The type of memory is specified as a program argument. This current array is then compared to the array of the thread that is one less than itself. The previous array is stored in global memory everytime. In the comparison, each thread conducts a linear search with the `target` being any of the elements in the current array. If a `target` is found, the search stops and it is denoted that a match has been found by writing a `1` at index of the thread's `id` in an array called `match`. The `match` array and all the global arrays are copied back to the host. Timing information is then printed in the form of a csv. See `data/empty_results.csv` to disambiguate the output. In debug mode, the experiment is performed sequentially by the CPU and compared against GPU run experiment. If there is an error it will print information to the standard error file stream. If in verbose debug mode all of the arrays will be printed for manual comparison.
+
+## Limitations
+Currently this only runs on one thread block with 1024 arrays.
+The total number of arrays must be less than 1024.
+The size of the arrays must be less than 1024.
 
 ## Requirements
 * NVIDIA Capable GPU
@@ -12,20 +19,20 @@ Independent Study with Dr. Junqiao Qiu Spring '21
 * (Optional) R for analysis script
 
 ## Build instructions
-This project relies on the 'nvcc' compiler and 'make' commands.
+This project relies on the `nvcc` compiler and `make` commands.
 
-To make the executable 'main':
-'make'
+To make the executable `main`:
+`make`
 
 To clean the directory of object files:
-'make clean'
+`make clean`
 
 To run a set of performance runs:
-'source performance.sh'
-which will output a series to the 'data' folder
+`source performance.sh`
+which will output a series to the `data` folder
 
 To run a single run of the program:
-'./main arg1 arg2 arg3 arg4'
+`./main arg1 arg2 arg3 arg4`
 
 Where the arugments are specified as follows:
 1) Argument 1: The size of each array (up to 1024)
@@ -34,7 +41,7 @@ Where the arugments are specified as follows:
 4) Argument 4: Debug option. 0 for no debugging, 1 for simple debug check, 2 or more for verbose check.
 
 ## Edit instructions
-* 'main.cu': main function that coordinates calls to the GPU
-* 'array_match.cu': function that conducts the array matching via global memory
+* `main.cu`: main function that coordinates calls to the GPU
+* `array_match.cu`: function that conducts the array matching via global memory
 
-* 'shm_array_match.cu': function that conducts the array matching via shared memory
+* `shm_array_match.cu`: function that conducts the array matching via shared memory
--- GPU_Array_Matching/analysis.R
+++ GPU_Array_Matching/analysis.R
@@ -1,5 +1,19 @@
 setwd("/home/rwatling/Academics/MTU/masters/Programming/CUDA_Programming/GPU_Array_Matching")
 
+#packages required
+packages = c("gbutils")
+
+## Now load or install&load all
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
+
 file1 <- "data/change_arr_size_small.csv"
 file2 <- "data/change_arr_size_avg.csv"
 file3 <- "data/change_arr_size_lg.csv"
@@ -8,46 +22,51 @@ file5 <- "data/change_num_arr_avg.csv"
 file6 <- "data/change_num_arr_lg.csv"
 
 ### Change Array Size: file1-3###
-changeArrSizeDf <- read.csv(file3)
+changeArrSizeDf <- read.csv(file1)
 sharedChangeArrSizeDf <- changeArrSizeDf[which(changeArrSizeDf$type == 1),]
 globalChangeArrSizeDf <- changeArrSizeDf[which(changeArrSizeDf$type == 0),]
 
 plot.new()
 plot(globalChangeArrSizeDf$array_size, globalChangeArrSizeDf$time, 
-     col="red", xlab="Array Size", ylab = "Time", main = "Time vs. Array Size", pch =19,
-     ylim=c(min(sharedChangeArrSizeDf$time), max(globalChangeArrSizeDf$time) + 0.005))
+     col="red", xlab="Array Size", ylab = "Time", main = "Time vs. Array Size", pch =19)
 points(sharedChangeArrSizeDf$array_size, sharedChangeArrSizeDf$time, col="blue", pch =19)
 
-lines(sharedChangeArrSizeDf$array_size, sharedChangeArrSizeDf$time, col="blue")
-lines(globalChangeArrSizeDf$array_size, globalChangeArrSizeDf$time, col="red")
+fit1 <- lm(globalChangeArrSizeDf$time ~ globalChangeArrSizeDf$array_size)
+abline(fit1, col = "red", lwd=2)
 
-fit <- lm(globalChangeArrSizeDf$time ~ globalChangeArrSizeDf$array_size)
-abline(fit)
+fit2 <- lm(sharedChangeArrSizeDf$time ~ sharedChangeArrSizeDf$array_size)
+abline(fit2, col = "blue", lwd=2)
 
-legend("topleft", legend=c("Global Mem", "Shared Mem", "Global Fit"),col=c("red", "blue", "black"), pch = c(19, 19, 19))
+legend("topleft", legend=c("Global Mem", "Shared Mem"),col=c("red", "blue"), pch = c(19, 19))
 
 #speed up
-mean(globalChangeArrSizeDf$time / sharedChangeArrSizeDf$time)
+speedup <- paste("Global Time / Shared Time =", toString(mean(globalChangeArrSizeDf$time / sharedChangeArrSizeDf$time)))
+mtext(speedup, side = 3)
 
-### Change number of arrays: files4-6 ###
+### Change number of arrays: files 4-6 ###
 changeArrNumDf <- read.csv(file4)
 sharedChangeArrNumDf <- changeArrNumDf[which(changeArrNumDf$type == 1),]
 globalChangeArrNumDf <- changeArrNumDf[which(changeArrNumDf$type == 0),]
 
 plot.new()
 plot(globalChangeArrNumDf$number_of_arrays, globalChangeArrNumDf$time, 
-     col="red", xlab="Array Size", ylab = "Time", main = "Time vs. Array Number", pch =19,
-     ylim=c(min(sharedChangeArrNumDf$time), max(globalChangeArrNumDf$time) + 0.005))
+     col="red", xlab="Array Size", ylab = "Time", main = "Time vs. Array Number", pch =19)
 points(sharedChangeArrNumDf$number_of_arrays, sharedChangeArrNumDf$time, col="blue", pch =19)
 
-lines(sharedChangeArrNumDf$number_of_arrays, sharedChangeArrNumDf$time, col="blue")
-lines(globalChangeArrNumDf$number_of_arrays, globalChangeArrNumDf$time, col="red")
+fit1 <- lm(globalChangeArrNumDf$time ~ globalChangeArrNumDf$array_size)
+if(gbutils::isNA(fit1$coefficients[2])) {
+  fit1$coefficients[2] <- 0
+}
+abline(fit1, col = "red", lwd=2)
 
-fit <- lm(globalChangeArrNumDf$time ~ globalChangeArrNumDf$number_of_arrays)
-abline(fit)
+fit2 <- lm(sharedChangeArrNumDf$time ~ sharedChangeArrNumDf$array_size)
+if(gbutils::isNA(fit2$coefficients[2] == "NA")) {
+  fit2$coefficients[2] = 0
+}
+abline(fit2, col = "blue", lwd=2)
 
-legend("topleft", legend=c("Global Mem", "Shared Mem", "Global Fit"),col=c("red", "blue", "black"), pch = c(19, 19, 19))
+legend("topleft", legend=c("Global Mem", "Shared Mem"),col=c("red", "blue"), pch = c(19, 19, 19))
 
 #speed up
-mean(globalChangeArrNumDf$time / sharedChangeArrNumDf$time)
-
+speedup <- paste("Global Time / Shared Time =", toString(mean(globalChangeArrNumDf$time / sharedChangeArrNumDf$time)))
+mtext(speedup, side = 3)
