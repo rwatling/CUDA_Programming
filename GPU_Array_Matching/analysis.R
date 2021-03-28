d21a9aece7beae523dee449c665cdefa676ae23a
Robbie Watling <rwatling@pop-os.localdomain>
2021-03-28 13:53:41 -0400

    Plots now use ggplot2

--- GPU_Array_Matching/analysis.R
+++ GPU_Array_Matching/analysis.R
@@ -1,7 +1,7 @@
 setwd("/home/rwatling/Academics/MTU/masters/Programming/CUDA_Programming/GPU_Array_Matching")
 
 #packages required
-packages = c("gbutils", "ggplot2")
+packages = c("gbutils", "ggplot2", "gghighlig")
 
 ## Now load or install&load all
 package.check <- lapply(
@@ -38,6 +38,36 @@ allPerfDf[which(allPerfDf$type == 0),]$type = "global"
 plot.new()
 ggplot(data = allPerfDf) +
   geom_smooth(mapping = aes(x = array_size, y = time, group = type, color=type)) +
-  geom_point(mapping = aes(x = array_size, y = time, color=type))
+  geom_point(mapping = aes(x = array_size, y = time, color=type)) +
+  ggtitle("Time vs. Array Size") +
+  xlab("Array Size") +
+  ylab("Time (ms)") +
+  theme_minimal()
 
 speedup = mean(globalChangeDf$time) / mean(sharedChangeDf$time)
+speedup
+
+### Change Array Number: file4-6###
+allPerfDf <- read.csv(all_files[4])
+allPerfDf <- allPerfDf[which(allPerfDf$time < 10000000),]
+
+# For speedup
+sharedChangeDf <-  allPerfDf[which(allPerfDf$type == 1),]
+globalChangeDf <-  allPerfDf[which(allPerfDf$type == 0),]
+
+# For plots
+allPerfDf[which(allPerfDf$type == 1),]$type = "shared"
+allPerfDf[which(allPerfDf$type == 0),]$type = "global"
+
+# Plot 
+plot.new()
+ggplot(data = allPerfDf) +
+  geom_smooth(mapping = aes(x = number_of_arrays, y = time, group = type, color=type)) +
+  geom_point(shape = 20, mapping = aes(x = number_of_arrays, y = time, color=type)) +
+  ggtitle("Time vs. Array Number") +
+  xlab("Array Number") +
+  ylab("Time (ms)") +
+  theme_minimal()
+
+speedup = mean(globalChangeDf$time) / mean(sharedChangeDf$time)
+speedup
