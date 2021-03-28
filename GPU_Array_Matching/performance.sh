ec88a47b134e0983a6d077c786351c419ba3f532
Robbie <rwatling0902@gmail.com>
2021-03-28 08:24:30 -0700

    performance run

--- GPU_Array_Matching/performance.sh
+++ GPU_Array_Matching/performance.sh
@@ -18,28 +18,28 @@ cp $empty $file6
 
 #Current maximum size is 1024 * 1024
 
-# array_size goes from 4 to 512 in increments of 4
-for i in {4..512..4}
+# array_size goes from 4 to 1024 in increments of 4
+for i in {4..1024..4}
 do
-	./main $((i)) 4 1 >> $file1
-	./main $((i)) 4 0 >> $file1
+	./main $((i)) 4 1 0 >> $file1
+	./main $((i)) 4 0 0 >> $file1
 
-	./main $((i)) 512 1 >> $file2
-	./main $((i)) 512 0 >> $file2
+	./main $((i)) 512 1 0 >> $file2
+	./main $((i)) 512 0 0 >> $file2
 
-	./main $((i)) 1024 1 >> $file3
-	./main $((i)) 1024 0 >> $file3
+	./main $((i)) 1024 1 0 >> $file3
+	./main $((i)) 1024 0 0 >> $file3
 done
 
-# num_arrays goes from 4 to 512 in increments of 4
-for i in {4..512..4}
+# num_arrays goes from 4 to 1024 in increments of 4
+for i in {4..1024..4}
 do
-	./main 4 $((i)) 1 >> $file4
-	./main 4 $((i)) 0 >> $file4
+	./main 4 $((i)) 1 0 >> $file4
+	./main 4 $((i)) 0 0 >> $file4
 
-	./main 512 $((i)) 1 >> $file5
-	./main 512 $((i)) 0 >> $file5
+	./main 512 $((i)) 1 0 >> $file5
+	./main 512 $((i)) 0 0 >> $file5
 
-	./main 1024 $((i)) 1 >> $file6
-	./main 1024 $((i)) 0 >> $file6
+	./main 1024 $((i)) 1 0 >> $file6
+	./main 1024 $((i)) 0 0 >> $file6
 done
