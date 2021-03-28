305af75faed97fd7286ef8e0dcb176d6b413e880
Robbie Watling <rwatling@pop-os.localdomain>
2021-03-28 08:51:13 -0400

    R script changes

--- GPU_Array_Matching/analysis.R
+++ GPU_Array_Matching/analysis.R
@@ -1,7 +1,7 @@
 setwd("/home/rwatling/Academics/MTU/masters/Programming/CUDA_Programming/GPU_Array_Matching")
 
 #packages required
-packages = c("gbutils")
+packages = c("gbutils", "ggplot2")
 
 ## Now load or install&load all
 package.check <- lapply(
@@ -20,54 +20,24 @@ file3 <- "data/change_arr_size_lg.csv"
 file4 <- "data/change_num_arr_small.csv"
 file5 <- "data/change_num_arr_avg.csv"
 file6 <- "data/change_num_arr_lg.csv"
+all_files <- c(file1, file2, file3, file4, file5, file6)
 
 ### Change Array Size: file1-3###
-changeArrSizeDf <- read.csv(file3)
-sharedChangeArrSizeDf <- changeArrSizeDf[which(changeArrSizeDf$type == 1),]
-globalChangeArrSizeDf <- changeArrSizeDf[which(changeArrSizeDf$type == 0),]
+allPerfDf <- read.csv(all_files[4])
+allPerfDf <- allPerfDf[which(allPerfDf$time < 1000000),]
 
+# For speedup
+sharedChangeDf <-  allPerfDf[which(allPerfDf$type == 1),]
+globalChangeDf <-  allPerfDf[which(allPerfDf$type == 0),]
+
+# For plots
+allPerfDf[which(allPerfDf$type == 1),]$type = "shared"
+allPerfDf[which(allPerfDf$type == 0),]$type = "global"
+
+# Plot 
 plot.new()
-plot(globalChangeArrSizeDf$array_size, globalChangeArrSizeDf$time, 
-     col="red", xlab="Array Size", ylab = "Time", main = "Time vs. Array Size", pch =20)
-points(sharedChangeArrSizeDf$array_size, sharedChangeArrSizeDf$time, col="blue", pch =20)
+ggplot(data = allPerfDf) +
+  geom_smooth(mapping = aes(x = array_size, y = time, group = type, color=type)) +
+  geom_point(mapping = aes(x = array_size, y = time, color=type))
 
-fit1 <- lm(globalChangeArrSizeDf$time ~ globalChangeArrSizeDf$array_size)
-abline(fit1, col = "red", lwd=2)
-
-fit2 <- lm(sharedChangeArrSizeDf$time ~ sharedChangeArrSizeDf$array_size)
-abline(fit2, col = "blue", lwd=2)
-
-legend("topleft", legend=c("Global Mem", "Shared Mem"),col=c("red", "blue"), pch = c(20, 20))
-
-#speed up
-speedup <- paste("Global Time / Shared Time =", toString(mean(globalChangeArrSizeDf$time / sharedChangeArrSizeDf$time)))
-mtext(speedup, side = 3)
-
-### Change number of arrays: files 4-6 ###
-changeArrNumDf <- read.csv(file6)
-sharedChangeArrNumDf <- changeArrNumDf[which(changeArrNumDf$type == 1),]
-globalChangeArrNumDf <- changeArrNumDf[which(changeArrNumDf$type == 0),]
-
-plot.new()
-plot(globalChangeArrNumDf$number_of_arrays, globalChangeArrNumDf$time, 
-     col="red", xlab="Array Size", ylab = "Time", main = "Time vs. Array Number", pch =20)
-points(sharedChangeArrNumDf$number_of_arrays, sharedChangeArrNumDf$time, col="blue", pch =20)
-
-fit1 <- lm(globalChangeArrNumDf$time ~ globalChangeArrNumDf$array_size)
-if(gbutils::isNA(fit1$coefficients[2])) {
-  fit1$coefficients[2] <- 0
-}
-abline(fit1, col = "red", lwd=2)
-
-fit2 <- lm(sharedChangeArrNumDf$time ~ sharedChangeArrNumDf$array_size)
-if(gbutils::isNA(fit2$coefficients[2] == "NA")) {
-  fit2$coefficients[2] = 0
-}
-abline(fit2, col = "blue", lwd=2)
-
-legend("topleft", legend=c("Global Mem", "Shared Mem"),col=c("red", "blue"), pch = c(20, 20, 20))
-
-#speed up
-speedup <- paste("Global Time / Shared Time =", toString(mean(globalChangeArrNumDf$time / sharedChangeArrNumDf$time)))
-mtext(speedup, side = 3)
-
+speedup = mean(globalChangeDf$time) / mean(sharedChangeDf$time)
