b9aed38ff311e5453472e7ec62e2371d5a03262f
Robbie Watling <rwatling@pop-os.localdomain>
2021-05-06 09:39:57 -0400

    project builds with Cmake now

---
+++ GPU_Array_Matching/CMakeFiles/gpu_match.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gpu_match.dir/src/array_match.cu.o"
+  "CMakeFiles/gpu_match.dir/src/main.cu.o"
+  "CMakeFiles/gpu_match.dir/src/shm_array_match.cu.o"
+  "gpu_match"
+  "gpu_match.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/gpu_match.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
