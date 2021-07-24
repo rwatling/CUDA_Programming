a1ff87e5f1cad9c1a6ce19920df6039e0998772d
Robbie Watling <rwatling@pop-os.localdomain>
2021-07-24 10:41:59 -0400

    naive even odd sort appears to be working correctly. Code cleaned up a bit.

--- GPU_Array_Matching/src/shfl_match.cu
+++ GPU_Array_Matching/src/shfl_match.cu
@@ -5,13 +5,13 @@ __global__ void shfl_match(int* all_arrays, int* match_array, int num_arrays, in
 	// Essential variables
 	int thread_id = (blockIdx.x * blockDim.x) + threadIdx.x;
 	int lane_id = threadIdx.x % WARP_SIZE;
-	int match = 0;
+	//int match = 0;
 	int prev_num = 0;
 	int current_num = 0;
 	int mask = 0xfffffff;
 
 	int* g_current_arr = all_arrays + (thread_id * size); //Pointer arithmetic
-	int* g_prev_arr = all_arrays + ((thread_id - 1) * size); //Pointer arithmetic
+	//int* g_prev_arr = all_arrays + ((thread_id - 1) * size); //Pointer arithmetic
 
 	int local_arr[64];
 
@@ -36,62 +36,98 @@ __global__ void shfl_match(int* all_arrays, int* match_array, int num_arrays, in
 	/* Needs:
 	-zero boundary
 	-global memory write X
-	-compilation
+	-mismatch at odd thread compare to even thread
+	-timing information
 	*/
 
-	// Current just psuedo code
-	for (int i = 0; i < 2; i ++) {
-		if (thread_id % 2 == 0) {
-			if (i == 0) {
+	// Variation of even odd sort
+	for (int i = 0; i < 2; i++) {
+
+		// Even pass
+		if (i == 0) {
+
+			/* Even thread generates its random numbers in local array
+			*  It participates in the shuffle for synchronization but ignores the result
+			*/
+			if (thread_id % 2 == 0) {
+
 				//Comparison
 				for (int i = 0; i < size; i++) {
 					for (int j = 0; j < size; j++) {
-						current_num = (int) (curand_uniform(&state) * maxRand);
-						g_current_arr[j] = current_num;
-						local_arr[j] = current_num;
+
+						// First pass of outer loop
+						if (i == 0) {
+							current_num = (int) (curand_uniform(&state) * maxRand);
+							g_current_arr[j] = current_num;
+							local_arr[j] = current_num;
+
+						// All other passes
+						} else {
+							current_num = local_arr[j];
+						}
 
 						prev_num = __shfl_sync(mask, current_num, lane_id - 1);
 
 						//ignore result
 					}
 				}
-			} else if (i == 1) {
-				//Comparison
-				for (int i = 0; i < size; i++) {
-					current_num = local_arr[i];
 
-					for (int j = 0; j < size; j++) {
-						prev_num = __shfl_sync(mask, current_num, lane_id - 1);
-
-						if (current_num == prev_num) {
-							match_array[thread_id] = 1;
-						}
-					}
-				}
-			}
-		} else if (thread_id % 2 == 1) {
-			if (i == 0) {
+			/*  Odd thread generates its random numbers in local array
+			 *  It participates in the shuffle, comparing its result
+			 */
+			} else if (thread_id % 2 == 1) {
 
 				//Comparison
 				for (int i = 0; i < size; i++) {
-					local_arr[i] = (int) (curand_uniform(&state) * maxRand);
-					current_num = local_arr[i];
+
+					current_num = (int) (curand_uniform(&state) * maxRand);
 					g_current_arr[i] = current_num;
+					local_arr[i] = current_num;
 
 					for (int j = 0; j < size; j++) {
+
 						prev_num = __shfl_sync(mask, current_num, lane_id - 1);
 
 						if (current_num == prev_num) {
 							match_array[thread_id] = 1;
 						}
+
 					}
 				}
-			} else if (i == 1) {
-				//Comparison
+
+			}
+
+		// Odd pass
+		} else if (i == 1) {
+
+			// Even thread
+			if (thread_id % 2 == 0) {
+
+				// Compares each previously generated even element to
+				// Previously generated odd element
+				for (int i = 0; i < size; i++) {
+
+					current_num = local_arr[i];
+
+					for (int j = 0; j < size; j++) {
+
+						prev_num = __shfl_sync(mask, current_num, lane_id - 1);
+
+						if ((current_num == prev_num) && (lane_id != 0)) {
+							match_array[thread_id] = 1;
+						}
+
+					}
+				}
+
+			// Odd thread
+			} else if (thread_id % 2 == 1) {
+
+				// Compared to each previously generated even element
 				for (int i = 0; i < size; i++) {
 					for (int j = 0; j < size; j++) {
-						current_num = local_arr[j];
 
+						current_num = local_arr[j];
 						prev_num = __shfl_sync(mask, current_num, lane_id - 1);
 
 						//ignore result
@@ -100,28 +136,6 @@ __global__ void shfl_match(int* all_arrays, int* match_array, int num_arrays, in
 			}
 		}
 	}
-
-	//Old code
-	/*match_array[thread_id] = 0;
-	for (int pass = 0; pass <= total_pass; pass++) {
-
-		__syncthreads();
-
-		//Generate num_regs random numbers
-		for (int i = 0; i < NUM_REGS; i++) {
-			current_num = (int) (curand_uniform(&state) * maxRand);
-			g_current_arr[total_count++] = current_num;
-		}
-
-		// Compare num_regs at a time?
-		for (int i = 0; i < NUM_REGS; i++) {
-			for (int j = 0; j < NUM_REGS; j++) {
-				prev_num = __shfl_sync(0xfffffff, current_num, lane_id + 1);
-				match_array[thread_id] = current_num;
-			}
-		}
-	}*/
-
 
 	__syncthreads();
 
