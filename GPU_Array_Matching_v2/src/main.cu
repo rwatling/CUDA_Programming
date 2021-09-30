e66dc6948d7f93253739345469166fb0856df8b6
Robbie Watling <rwatling@pop-os.localdomain>
2021-09-29 21:19:28 -0400

    bug on JQ server version. Chaning shared memory size

--- GPU_Array_Matching_v2/src/main.cu
+++ GPU_Array_Matching_v2/src/main.cu
@@ -11,19 +11,22 @@
 #include <utility>
 #include <iostream>
 #include <iomanip>
-#include <ctime>
+#include <time.h>
 
 using namespace std;
 
 // For shuffling host arrays
 void shuffle(int *array, size_t n)
 {
+    if (clock() == -1) {
+      cerr << "Clock failed" << endl;
+    }
+
     srand(clock());
-    if (n > 1)
-    {
+
+    if (n > 1) {
         size_t i;
-        for (i = 0; i < n - 1; i++)
-        {
+        for (i = 0; i < n - 1; i++){
           size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
           int t = array[j];
           array[j] = array[i];
