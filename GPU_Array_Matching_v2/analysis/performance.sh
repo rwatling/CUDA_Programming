718a60250eeb9005f9822d4e7c221854732b7752
rwatling <rwatling0902@gmail.com>
2021-10-15 10:01:35 -0700

    performance runs with less shuffle syncs

--- GPU_Array_Matching_v2/analysis/performance.sh
+++ GPU_Array_Matching_v2/analysis/performance.sh
@@ -6,8 +6,8 @@ empty=./data/empty_results.csv
 
 cp $empty $file1
 
-# array_size goes from 4 to 1024 in increments of 4
-for i in {4..1024..4}
+# array_size goes from 8 to 1024 in increments of 8
+for i in {8..1024..8}
 do
 	../gpu_match $((i)) 0 >> $file1
 	../gpu_match $((i)) 0 >> $file1
