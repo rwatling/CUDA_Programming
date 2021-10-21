a7d5ab05ea27f6263f1765a92d87dfb88736fecf
rwatling <rwatling0902@gmail.com>
2021-10-21 07:31:19 -0700

    performance run size 24

--- GPU_Array_Matching_v2/analysis/performance.sh
+++ GPU_Array_Matching_v2/analysis/performance.sh
@@ -6,7 +6,7 @@ empty=./data/empty_results.csv
 
 cp $empty $file1
 
-# array_size goes from 8 to 1024 in increments of 8
+# number of threads goes from 8 to 1024 in increments of 8
 for i in {8..1024..8}
 do
 	../gpu_match $((i)) 0 >> $file1
