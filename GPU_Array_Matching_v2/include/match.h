f766b6f023becc8326f0bccbe9f9d3e897487aa9
rwatling <rwatling0902@gmail.com>
2021-10-21 07:24:14 -0700

    compile with size 24

--- GPU_Array_Matching_v2/include/match.h
+++ GPU_Array_Matching_v2/include/match.h
@@ -1,6 +1,6 @@
 #ifndef MATCH_H
 #define MATCH_H 1
-#define ARRAY_SIZE 12
+#define ARRAY_SIZE 24
 #include "cuda_includes.h"
 __device__ void match(int* array2, int* next_arr1, int* next_arr2);
 #endif
