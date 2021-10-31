ccbdce43cac6af198fa71ce604dd4e755f96d628
Robbie Watling <rwatling@pop-os.localdomain>
2021-10-30 20:57:47 -0400

    analysis updates

--- GPU_Array_Matching_v2/analysis/analysis.R
+++ GPU_Array_Matching_v2/analysis/analysis.R
@@ -14,79 +14,215 @@ package.check <- lapply(
   }
 )
 
-file1 <- "data/change_t_a4.csv"
-file2 <- "data/change_t_a8.csv"
-file3 <- "data/change_t_a12.csv"
-file4 <- "data/change_t_a16.csv"
-file5 <- "data/change_t_a24.csv"
-all_files <- c(file1, file2, file3, file4, file5)
+file1 <- "data/hash/sm_hash_array_size4.csv"
+file2 <- "data/hash/sm_hash_array_size4.csv"
+file3 <- "data/hash/sm_hash_array_size4.csv"
+file4 <- "data/hash/sm_hash_array_size16.csv"
+file5 <- "data/hash/sm_hash_array_size24.csv"
 
 ### Change Number of Threads ###
-allPerfDf1 <- read.csv(all_files[1])
-allPerfDf2 <- read.csv(all_files[2])
-allPerfDf3 <- read.csv(all_files[3])
-allPerfDf4 <- read.csv(all_files[4])
-allPerfDf5 <- read.csv(all_files[5])
+naiveArr4Df <- read.csv(file1)
+naiveArr8Df <- read.csv(file2)
+naiveArr12Df <- read.csv(file3)
+naiveArr16Df <- read.csv(file4)
+naiveArr24Df <- read.csv(file5)
 
 # Filter data
-allPerfDf1[which(allPerfDf1$type == 1),]$type = "shuffle"
-allPerfDf1[which(allPerfDf1$type == 0),]$type = "shared"
+temp1 <- naiveArr4Df[which(naiveArr4Df$type == 2),]
+temp2 <- naiveArr4Df[which(naiveArr4Df$type == 1),]
+temp1[which(temp1$type == 2),]$type = "shuffle"
+temp2[which(temp2$type == 1),]$type = "shared"
+naive4 <- rbind(temp1, temp2)
 
-allPerfDf2[which(allPerfDf2$type == 1),]$type = "shuffle"
-allPerfDf2[which(allPerfDf2$type == 0),]$type = "shared"
+temp1 <- naiveArr8Df[which(naiveArr8Df$type == 2),]
+temp2 <- naiveArr8Df[which(naiveArr8Df$type == 1),]
+temp1[which(temp1$type == 2),]$type = "shuffle"
+temp2[which(temp2$type == 1),]$type = "shared"
+naive8 <- rbind(temp1, temp2)
 
-allPerfDf3[which(allPerfDf3$type == 1),]$type = "shuffle"
-allPerfDf3[which(allPerfDf3$type == 0),]$type = "shared"
+temp1 <- naiveArr12Df[which(naiveArr12Df$type == 2),]
+temp2 <- naiveArr12Df[which(naiveArr12Df$type == 1),]
+temp1[which(temp1$type == 2),]$type = "shuffle"
+temp2[which(temp2$type == 1),]$type = "shared"
+naive12 <- rbind(temp1, temp2)
 
-allPerfDf4[which(allPerfDf4$type == 1),]$type = "shuffle"
-allPerfDf4[which(allPerfDf4$type == 0),]$type = "shared"
+temp1 <- naiveArr16Df[which(naiveArr16Df$type == 2),]
+temp2 <- naiveArr16Df[which(naiveArr16Df$type == 1),]
+temp1[which(temp1$type == 2),]$type = "shuffle"
+temp2[which(temp2$type == 1),]$type = "shared"
+naive16 <- rbind(temp1, temp2)
 
-allPerfDf5[which(allPerfDf5$type == 1),]$type = "shuffle"
-allPerfDf5[which(allPerfDf5$type == 0),]$type = "shared"
+temp1 <- naiveArr24Df[which(naiveArr24Df$type == 2),]
+temp2 <- naiveArr24Df[which(naiveArr24Df$type == 1),]
+temp1[which(temp1$type == 2),]$type = "shuffle"
+temp2[which(temp2$type == 1),]$type = "shared"
+naive24 <- rbind(temp1, temp2)
 
 # Combine 1-5 for line graph
-combined <- rbind(allPerfDf1, allPerfDf2, allPerfDf3, allPerfDf4, allPerfDf5)
+combined <- rbind(naive4, naive8, naive12, naive16, naive24)
 
 # Plot
 png("shared_vs_shuffle_line.png")
 plot.new()
 ggplot() +
-  geom_smooth(data = combined, mapping = aes(x = number_of_arrays, y = time, group=interaction(type, array_size), color=interaction(type, array_size))) +
+  geom_smooth(data = combined, mapping = aes(x = number_of_arrays, y = time, group=interaction(type, array_size), color=interaction(type, array_size)), se=F) +
   ggtitle("Time vs. Number of Threads") +
   ylab("Time (ms)") +
+  xlab("Number of Arrays") +
   theme_minimal() +
   scale_color_brewer("Type | Arrays Size", palette = "Paired")
 dev.off()
 
-### Speedup information ###
-shuffleChangeDf1 <-  allPerfDf1[which(allPerfDf1$type == "shuffle"),]
-shareChangeDf1 <-  allPerfDf1[which(allPerfDf1$type == "shared"),]
+### HASH ANALYSIS ###
+sm_hash_a4 <- read.csv("data/hash/sm_hash_array_size4.csv")
+sm_hash_a8 <- read.csv("data/hash/sm_hash_array_size8.csv")
+sm_hash_a12 <- read.csv("data/hash/sm_hash_array_size12.csv")
+sm_hash_a16 <- read.csv("data/hash/sm_hash_array_size16.csv")
+sm_hash_a24 <- read.csv("data/hash/sm_hash_array_size24.csv")
 
-shuffleChangeDf2 <-  allPerfDf2[which(allPerfDf2$type == "shuffle"),]
-shareChangeDf2 <-  allPerfDf2[which(allPerfDf2$type == "shared"),]
+lg_hash_a4 <- read.csv("data/hash/lg_hash_array_size4.csv")
+lg_hash_a8 <- read.csv("data/hash/lg_hash_array_size8.csv")
+lg_hash_a12 <- read.csv("data/hash/lg_hash_array_size12.csv")
+lg_hash_a16 <- read.csv("data/hash/lg_hash_array_size16.csv")
+lg_hash_a24 <- read.csv("data/hash/lg_hash_array_size24.csv")
 
-shuffleChangeDf3 <-  allPerfDf3[which(allPerfDf3$type == "shuffle"),]
-shareChangeDf3 <-  allPerfDf3[which(allPerfDf3$type == "shared"),]
+### Base shuffle vs small hash ###
+# Filter data
+temp1 <- sm_hash_a4[which(sm_hash_a4$type == 2),]
+temp2 <- sm_hash_a4[which(sm_hash_a4$type == 4),]
+sm_hash_v_shfl4 <- rbind(temp1, temp2)
+sm_hash_v_shfl4[which(sm_hash_v_shfl4 $type == 2),]$type = "shfl"
+sm_hash_v_shfl4 [which(sm_hash_v_shfl4 $type == 4),]$type = "hash"
 
-shuffleChangeDf4 <-  allPerfDf1[which(allPerfDf4$type == "shuffle"),]
-shareChangeDf4 <-  allPerfDf1[which(allPerfDf4$type == "shared"),]
+temp1 <- sm_hash_a8[which(sm_hash_a8$type == 2),]
+temp2 <- sm_hash_a8[which(sm_hash_a8$type == 4),]
+sm_hash_v_shfl8 <- rbind(temp1, temp2)
+sm_hash_v_shfl8[which(sm_hash_v_shfl8$type == 2),]$type = "shfl"
+sm_hash_v_shfl8 [which(sm_hash_v_shfl8$type == 4),]$type = "hash"
 
-shuffleChangeDf5 <-  allPerfDf1[which(allPerfDf5$type == "shuffle"),]
-shareChangeDf5 <-  allPerfDf1[which(allPerfDf5$type == "shared"),]
+temp1 <- sm_hash_a12[which(sm_hash_a12$type == 2),]
+temp2 <- sm_hash_a12[which(sm_hash_a12$type == 4),]
+sm_hash_v_shfl12 <- rbind(temp1, temp2)
+sm_hash_v_shfl12[which(sm_hash_v_shfl12$type == 2),]$type = "shfl"
+sm_hash_v_shfl12[which(sm_hash_v_shfl12$type == 4),]$type = "hash"
 
-# Mean
-meanTimeDf1 <- mean(shareChangeDf1$time) / mean(shuffleChangeDf1$time)
-meanTimeDf2 <- mean(shareChangeDf2$time) / mean(shuffleChangeDf2$time)
-meanTimeDf3 <- mean(shareChangeDf3$time) / mean(shuffleChangeDf3$time)
-meanTimeDf4 <- mean(shareChangeDf4$time) / mean(shuffleChangeDf4$time)
-meanTimeDf5 <- mean(shareChangeDf5$time) / mean(shuffleChangeDf5$time)
+temp1 <- sm_hash_a16[which(sm_hash_a16$type == 2),]
+temp2 <- sm_hash_a16[which(sm_hash_a16$type == 4),]
+sm_hash_v_shfl16 <- rbind(temp1, temp2)
+sm_hash_v_shfl16[which(sm_hash_v_shfl16$type == 2),]$type = "shfl"
+sm_hash_v_shfl16[which(sm_hash_v_shfl16$type == 4),]$type = "hash"
 
-# Median
-medianTimeDf1 <- median(shareChangeDf1$time) / median(shuffleChangeDf1$time)
-medianTimeDf2 <- median(shareChangeDf2$time) / median(shuffleChangeDf2$time)
-medianTimeDf3 <- median(shareChangeDf3$time) / median(shuffleChangeDf3$time)
-medianTimeDf4 <- median(shareChangeDf4$time) / median(shuffleChangeDf4$time)
-medianTimeDf5 <- median(shareChangeDf5$time) / median(shuffleChangeDf5$time)
+temp1 <- sm_hash_a24[which(sm_hash_a24$type == 2),]
+temp2 <- sm_hash_a24[which(sm_hash_a24$type == 4),]
+sm_hash_v_shfl24 <- rbind(temp1, temp2)
+sm_hash_v_shfl24[which(sm_hash_v_shfl24$type == 2),]$type = "shfl"
+sm_hash_v_shfl24 [which(sm_hash_v_shfl24$type == 4),]$type = "hash"
+
+combined <- rbind(sm_hash_v_shfl4, sm_hash_v_shfl8, sm_hash_v_shfl12, sm_hash_v_shfl16, sm_hash_v_shfl24)
+
+# Plot
+png("small_hash_line.png")
+plot.new()
+ggplot() +
+  geom_smooth(data = combined, mapping = aes(x = number_of_arrays, y = time, group=interaction(type, array_size), color=interaction(type, array_size)), se=F) +
+  ggtitle("Time vs. Number of Threads") +
+  ylab("Time (ms)") +
+  xlab("Number of Arrays") +
+  theme_minimal() +
+  scale_color_brewer("Type | Arrays Size", palette = "Paired")
+dev.off()
+
+# Base shuffle vs large hash #
+temp1 <- lg_hash_a4[which(lg_hash_a4$type == 2),]
+temp2 <- lg_hash_a4[which(lg_hash_a4$type == 4),]
+lg_hash_v_shfl4 <- rbind(temp1, temp2)
+lg_hash_v_shfl4[which(lg_hash_v_shfl4 $type == 2),]$type = "shfl"
+lg_hash_v_shfl4 [which(lg_hash_v_shfl4 $type == 4),]$type = "hash"
+
+temp1 <- lg_hash_a8[which(lg_hash_a8$type == 2),]
+temp2 <- lg_hash_a8[which(lg_hash_a8$type == 4),]
+lg_hash_v_shfl8 <- rbind(temp1, temp2)
+lg_hash_v_shfl8[which(lg_hash_v_shfl8$type == 2),]$type = "shfl"
+lg_hash_v_shfl8 [which(lg_hash_v_shfl8$type == 4),]$type = "hash"
+
+temp1 <- lg_hash_a12[which(lg_hash_a12$type == 2),]
+temp2 <- lg_hash_a12[which(lg_hash_a12$type == 4),]
+lg_hash_v_shfl12 <- rbind(temp1, temp2)
+lg_hash_v_shfl12[which(lg_hash_v_shfl12$type == 2),]$type = "shfl"
+lg_hash_v_shfl12[which(lg_hash_v_shfl12$type == 4),]$type = "hash"
+
+temp1 <- lg_hash_a16[which(lg_hash_a16$type == 2),]
+temp2 <- lg_hash_a16[which(lg_hash_a16$type == 4),]
+lg_hash_v_shfl16 <- rbind(temp1, temp2)
+lg_hash_v_shfl16[which(lg_hash_v_shfl16$type == 2),]$type = "shfl"
+lg_hash_v_shfl16[which(lg_hash_v_shfl16$type == 4),]$type = "hash"
+
+temp1 <- lg_hash_a24[which(lg_hash_a24$type == 2),]
+temp2 <- lg_hash_a24[which(lg_hash_a24$type == 4),]
+lg_hash_v_shfl24 <- rbind(temp1, temp2)
+lg_hash_v_shfl24[which(lg_hash_v_shfl24$type == 2),]$type = "shfl"
+lg_hash_v_shfl24 [which(lg_hash_v_shfl24$type == 4),]$type = "hash"
+
+combined <- rbind(lg_hash_v_shfl4, lg_hash_v_shfl8, lg_hash_v_shfl12, lg_hash_v_shfl16, lg_hash_v_shfl24)
+
+# Plot
+png("large_hash_line.png")
+plot.new()
+ggplot() +
+  geom_smooth(data = combined, mapping = aes(x = number_of_arrays, y = time, group=interaction(type, array_size), color=interaction(type, array_size)), se=F) +
+  ggtitle("Time vs. Number of Threads") +
+  ylab("Time (ms)") +
+  xlab("Number of Arrays") +
+  theme_minimal() +
+  scale_color_brewer("Type | Arrays Size", palette = "Paired")
+dev.off()
+
+# Small hash vs large hash #
+temp1 <- sm_hash_a4[which(sm_hash_a4$type == 4),]
+temp2 <- lg_hash_a4[which(lg_hash_a4$type == 4),]
+temp1[which(temp1$type == 4),]$type = "small"
+temp2[which(temp2$type == 4),]$type = "large"
+sm_hash_v_lg_hash4 <- rbind(temp1, temp2)
+
+temp1 <- sm_hash_a8[which(sm_hash_a8$type == 4),]
+temp2 <- lg_hash_a8[which(lg_hash_a8$type == 4),]
+temp1[which(temp1$type == 4),]$type = "small"
+temp2[which(temp2$type == 4),]$type = "large"
+sm_hash_v_lg_hash8 <- rbind(temp1, temp2)
+
+temp1 <- sm_hash_a16[which(sm_hash_a16$type == 4),]
+temp2 <- lg_hash_a16[which(lg_hash_a16$type == 4),]
+temp1[which(temp1$type == 4),]$type = "small"
+temp2[which(temp2$type == 4),]$type = "large"
+sm_hash_v_lg_hash16 <- rbind(temp1, temp2)
+
+temp1 <- sm_hash_a12[which(sm_hash_a12$type == 4),]
+temp2 <- lg_hash_a12[which(lg_hash_a12$type == 4),]
+temp1[which(temp1$type == 4),]$type = "small"
+temp2[which(temp2$type == 4),]$type = "large"
+sm_hash_v_lg_hash12 <- rbind(temp1, temp2)
+
+temp1 <- sm_hash_a24[which(sm_hash_a24$type == 4),]
+temp2 <- lg_hash_a24[which(lg_hash_a24$type == 4),]
+temp1[which(temp1$type == 4),]$type = "small"
+temp2[which(temp2$type == 4),]$type = "large"
+sm_hash_v_lg_hash24 <- rbind(temp1, temp2)
+
+combined <- rbind(sm_hash_v_lg_hash4, sm_hash_v_lg_hash8, sm_hash_v_lg_hash12, sm_hash_v_lg_hash16, sm_hash_v_lg_hash24)
+
+# Plot
+png("small_vs_large_hash.png")
+plot.new()
+ggplot() +
+  geom_smooth(data = combined, mapping = aes(x = number_of_arrays, y = time, group=interaction(type, array_size), color=interaction(type, array_size)), se=F) +
+  ggtitle("Time vs. Number of Threads") +
+  ylab("Time (ms)") +
+  xlab("Number of Arrays") +
+  theme_minimal() +
+  scale_color_brewer("Type | Arrays Size", palette = "Paired")
+dev.off()
+
+### Speedup ###
 
 # Assemble data frame
 mean_speedup <- c(meanTimeDf1, meanTimeDf2, meanTimeDf3, meanTimeDf4, meanTimeDf5)
