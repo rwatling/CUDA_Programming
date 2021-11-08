5ead20bdaaa253079f063bf6a2ee1c158411e9a3
Robbie Watling <rwatling@pop-os.localdomain>
2021-11-08 11:05:30 -0500

    changed performance.sh

--- GPU_Array_Matching_v2/analysis/performance.sh
+++ GPU_Array_Matching_v2/analysis/performance.sh
@@ -6,8 +6,10 @@ empty=./data/empty_results.csv
 
 cp $empty $1
 
-# number of threads goes from 2 to 1024 in powers of 2
+# number of threads goes from 2 to 1024 in powers of 2 with 3 runs at each number of threads
 for i in {1..5}
 do
+	../gpu_match $((2**i)) >> $1
+	../gpu_match $((2**i)) >> $1
 	../gpu_match $((2**i)) >> $1
 done
