d9782a4d1ab470bb961df24b9fc74b4daa5b5263
Robbie <rwatling0902@gmail.com>
2021-11-08 08:47:08 -0800

    performance run

--- GPU_Array_Matching_v2/analysis/performance.sh
+++ GPU_Array_Matching_v2/analysis/performance.sh
@@ -7,7 +7,7 @@ empty=./data/empty_results.csv
 cp $empty $1
 
 # number of threads goes from 2 to 1024 in powers of 2 with 3 runs at each number of threads
-for i in {1..5}
+for i in {1..10}
 do
 	../gpu_match $((2**i)) >> $1
 	../gpu_match $((2**i)) >> $1
